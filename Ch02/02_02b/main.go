bbcd821453b03e4b830db39927b9279479eb9c41
Rafael Duarte <36821867+Rafabro8500@users.noreply.github.com>
2025-02-20 23:31:31 +0000

    25 variables

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,15 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
-
-	fmt.Println("Hello from Go!")
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	n := 25
+	fmt.Println(str1, str2, str3)
+	strLen, err := fmt.Println("Eu sou o ", n)
+	if err != nil {
+		fmt.Println("Error dred")
+	}
+	fmt.Println("String Length:", strLen)
+	fmt.Printf("O NUMERO SUPREMO É O %v\n", n)
 }
