f522a8a2b26a28870d9d96b8918e4550ac52c8c7
Rafael Duarte <36821867+Rafabro8500@users.noreply.github.com>
2025-02-21 00:17:30 +0000

    Coltxi inputs

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please input a string, Coltxi: ")
+	str, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(str)
+
+	fmt.Println("Please input number, Coltxi: ")
+	str, err = reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	n, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println("Who is against the queen, will die! ", err)
+	} else {
+		fmt.Println("Your number is: ", n)
+	}
 }
